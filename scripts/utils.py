8373a3483647b1ac15749eb8497413838aaab7b5
m4vlis <omario.pereira96@gmail.com>
2025-05-02 03:33:17 -0300

    feat: Novas funções inseridas. (editar_contatos, validar_email e validar_telefone).

--- scripts/utils.py
+++ scripts/utils.py
@@ -30,23 +30,22 @@ def cadastrar_contato(lista_contatos):
 
         while True:
             telefone = input('Telefone: ').strip().replace('-', '')
-            if len(telefone) < 11 or not telefone.isdigit():
-                print('Telefone inválido.')
+            telefone_validado = validar_telefone(telefone)
+            if telefone_validado == 'Telefone inválido.':
                 continue
-            telefone = f'({telefone[:2]}) {telefone[2:7]}-{telefone[7:]}'
             break
-
         while True:
             email = str(input('E-mail: '))
-            if '@' in email and '.' in email:
-                break
-            print('E-mail inválido.')
+            email_validado = validar_email(email)     
+            if email_validado == 'E-mail inválido, digite novamente.':
+                continue
+            break
         
         contato  = {
             'ID': str(uuid.uuid4()),
             'Nome': nome,
-            'Telefone': telefone,
-            'Email': email
+            'Telefone': telefone_validado,
+            'Email': email_validado
         }
         lista_contatos.append(contato)
         print('\n✅ Contato cadastrado com sucesso.')
@@ -83,7 +82,8 @@ def exibir_menu():
     print('2 - Listar todos os contatos')
     print('3 - Buscar contato')
     print('4 - Excluir contato')
-    print('5 - Sair')
+    print('5 - Editar contato')
+    print('6 - Sair')
 
 def obter_opcao_usuario():
     while True:
@@ -99,14 +99,15 @@ def obter_funcoes_menu():
     funcoes = {1:cadastrar_contato,
                2:listar_contatos,
                3:buscar_contatos,
-               4:excluir_contato}
+               4:excluir_contato,
+               5:editar_contato}
     return funcoes
 
 def menu_principal(lista_contatos):
     while True:
         escolha = obter_opcao_usuario()
         funcoes = obter_funcoes_menu()
-        if escolha == 5:
+        if escolha == 6:
             print('Obrigado por usar o ContatON CLI. Até logo!')
             break
         if escolha in funcoes.keys():
@@ -129,9 +130,8 @@ def buscar_contatos(lista_contatos):
                 print('Digite um termo válido.')
                 continue
             if campo_escolhido == 'email':
-                if '@' not in termo_busca and '.' not in termo_busca:
-                        print('Digite um e-mail válido.')
-                        continue
+                validar_email(termo_busca)
+
             resultado = [contato for contato in lista_contatos if termo_busca in contato[campo_escolhido.capitalize()].lower()]
             if len(resultado) == 0:
                 print('\n❌ Nenhum contato encontrado.')
@@ -171,10 +171,92 @@ def excluir_contato(lista_contatos):
         if pergunta == 'S':
             del lista_contatos[posicao]
             lista_contatos = sorted(lista_contatos, key=lambda x: x['Nome'])
-            with open (FILE_CONTACTS, 'w', encoding='utf-8') as f:
-                f = json.dump(lista_contatos, f, indent=4)
+            salvar_contatos(lista_contatos)
             print('✅ Contato excluído com sucesso!')
             return True
         else:
             print('\nOK, exclusão de contato cancelada.')
             return False
+
+def editar_contato(lista_contatos):
+    spinner('Listando contatos', 10)
+    listar_contatos(lista_contatos)
+    while True:
+        idContato = str(input('\nDigite o ID do contato que deseja alterar (ou Enter para voltar ao menu principal): ')).strip().lower()
+
+        if not idContato:
+            return
+    
+        contato = [(key, value) for key, value in enumerate(lista_contatos) if value['ID'].lower() == idContato]
+        if len(contato) == 0:
+            print('❌ Nenhum contato encontrado, tente novamente.')
+            continue
+        posicao = contato[0][0]
+        value = contato[0][1]
+        nome = value['Nome']
+        telefone = value['Telefone']
+        email = value['Email']
+
+        print('\nContato encontrado:\n')
+        print('🔹Contato')
+        print('Nome:'.ljust(10), nome)
+        print('Telefone:'.ljust(10), telefone)
+        print('E-mail:'.ljust(10), email)
+
+        novo_nome = str(input(f'\nNome atual: {nome} | Novo nome (ou Enter para manter): '))
+        if novo_nome:
+            value['Nome'] = novo_nome
+
+        novo_telefone = str(input(f'Telefone atual: {telefone} | Novo telefone (ou Enter para manter): '))
+        if validar_telefone(novo_telefone):
+            value['Telefone'] = novo_telefone
+
+        novo_email = str(input(f'E-mail atual: {email} | Novo e-mail (ou Enter para manter): '))
+        if validar_email(novo_email):
+            value['Email'] = novo_email
+
+        lista_contatos[posicao] = value
+        lista_contatos = sorted(lista_contatos, key=lambda x: x['Nome'])
+        salvar_contatos(lista_contatos)
+        print('\n✅ Contato alterado com sucesso.')
+        return
+    
+def validar_email(email):
+    while True:
+        if not email:
+            pergunta = input('E-mail em branco, deseja manter? (S/N)').strip().upper()
+            if pergunta == 'S':
+                return None
+            else:
+                continue
+        if '@' in email and '.' in email:
+            return email
+        else:
+            erro = 'E-mail inválido, digite novamente.'
+            return erro
+            
+
+def validar_telefone(telefone):
+    while True:
+        if not telefone:
+            pergunta = input('Telefone em branco, deseja manter? (S/N)').strip().upper()
+            if pergunta == 'S':
+                return None
+            else:
+                continue
+        if len(telefone) < 11 or not telefone.isdigit():
+            erro = 'Telefone inválido.'
+            return erro
+            
+        telefone = f'({telefone[:2]}) {telefone[2:7]}-{telefone[7:]}'
+        return telefone
+
+def spinner(texto, qtdVoltas):
+    from time import sleep
+    pins = ['|', '/', '-', '\\']
+
+    for volta in range(qtdVoltas):
+        for i in pins:
+            print(texto, i, end='\r')
+            sleep(0.1)
+    
